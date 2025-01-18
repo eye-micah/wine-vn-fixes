3ad689eb7c924c6546d9211050a30c3149cc1c26
Micah Daniel <kenzan@mailbox.org>
2025-01-18 15:36:15 -0500

    confused order

--- muramasa/muramasa_installer.sh
+++ muramasa/muramasa_installer.sh
@@ -68,9 +68,9 @@ install_wine() {
     
     local codename
     if [[ ! -f /etc/upstream-release/lsb-release ]]; then
-        codename=$(grep "^DISTRIB_CODENAME=" /etc/upstream-release/lsb-release | cut -d'=' -f2)
-    else
 	codename=$(lsb_release -cs | awk '{print $2}')
+    else
+	codename=$(grep "^DISTRIB_CODENAME=" /etc/upstream-release/lsb-release | cut -d'=' -f2)
     fi
     case "$distro" in
         debian | ubuntu)
