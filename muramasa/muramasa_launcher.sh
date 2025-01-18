3ee9627b3d48559205441bfe9e794972b9c3f3d4
Micah Daniel <kenzan@mailbox.org>
2025-01-18 11:06:38 -0500

    maybe it's a locale thing?

--- muramasa/muramasa_launcher.sh
+++ muramasa/muramasa_launcher.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 
 # Define Wine prefix and executable path
 WINEPREFIX="$HOME/.wine-muramasa"
-GAME_EXECUTABLE="$WINEPREFIX/drive_c/Program Files/Full Metal Daemon Muramasa/muramasa_en.exe"
+GAME_DIR="$WINEPREFIX/drive_c/Program Files/Full Metal Daemon Muramasa"
+GAME_EXECUTABLE="muramasa_en.exe"
+
+
 
 # Check if the game executable exists
 if [[ ! -f "$GAME_EXECUTABLE" ]]; then
@@ -14,5 +17,6 @@ fi
 
 # Run the game with WINEESYNC and WINEFSYNC disabled
 echo "Launching Full Metal Daemon Muramasa..."
-WINEDLLOVERRIDES="xaudio2_8=n" WINEESYNC=0 WINEFSYNC=0 WINEPREFIX="$WINEPREFIX" wine "$GAME_EXECUTABLE"
+cd $GAME_DIR
+LANG=ja_JP.UTF-8 WINEDLLOVERRIDES="xaudio2_8=n" WINEESYNC=0 WINEFSYNC=0 WINEPREFIX="$WINEPREFIX" wine "$GAME_EXECUTABLE"
 
