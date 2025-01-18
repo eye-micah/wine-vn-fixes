5b64de4503925f0c0596871fde220fcb3349ce13
Micah Daniel <kenzan@mailbox.org>
2025-01-18 14:28:58 -0500

    added winehq key import

--- muramasa/muramasa_installer.sh
+++ muramasa/muramasa_installer.sh
@@ -67,6 +67,8 @@ install_wine() {
         debian | ubuntu)
             sudo dpkg --add-architecture i386
             sudo apt update
+	    sudo mkdir -pm755 /etc/apt/keyrings
+	    wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
             sudo wget -NP /etc/apt/sources.list.d/ "https://dl.winehq.org/wine-builds/ubuntu/dists/${codename}/winehq-${codename}.sources"
             sudo apt update
             sudo apt install -y winehq-stable
