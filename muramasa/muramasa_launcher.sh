81c4bbf4dcf34efe0ad855f3828f1f4f2baae495
Micah Daniel <kenzan@mailbox.org>
2025-01-18 14:06:38 -0500

    properly double quoted

--- muramasa/muramasa_launcher.sh
+++ muramasa/muramasa_launcher.sh
@@ -10,6 +10,6 @@ GAME_EXECUTABLE="muramasa_en.exe"
 
 # Run the game with WINEESYNC and WINEFSYNC disabled
 echo "Launching Full Metal Daemon Muramasa..."
-cd ""$GAME_DIR""
+cd '"$GAME_DIR"'
 LANG=ja_JP.UTF-8 WINEDLLOVERRIDES="xaudio2_8=n,winegstreamer=" WINEESYNC=0 WINEFSYNC=0 WINEPREFIX="$WINEPREFIX" wine "$GAME_EXECUTABLE"
 
